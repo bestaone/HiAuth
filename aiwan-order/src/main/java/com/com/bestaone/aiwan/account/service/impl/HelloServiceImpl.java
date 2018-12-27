9ce62460942e915d5eabcd2edfaaa28a00322d6c
guosheng_zhang <guosheng_zhang@ssic.cn>
2018-12-27 10:27:36 +0800

    添加模块

---
+++ aiwan-order/src/main/java/com/com/bestaone/aiwan/account/service/impl/HelloServiceImpl.java
@@ -0,0 +1,15 @@
+package com.com.bestaone.aiwan.account.service.impl;
+
+import com.com.bestaone.aiwan.account.service.HelloService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloServiceImpl implements HelloService {
+
+	@Override
+	public String greeting(String name) {
+		System.out.println("greeting");
+		return "hello "+name;
+	}
+
+}
