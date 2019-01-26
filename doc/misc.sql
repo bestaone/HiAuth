7fa2d7d36551302af5bd1f72c962374880b52431
guosheng_zhang <guosheng_zhang@ssic.cn>
2019-01-26 13:52:54 +0800

    数据库

---
+++ doc/misc.sql
@@ -0,0 +1,32 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 127.0.0.1
+Source Server Version : 50519
+Source Host           : 127.0.0.1:3306
+Source Database       : misc
+
+Target Server Type    : MYSQL
+Target Server Version : 50519
+File Encoding         : 65001
+
+Date: 2019-01-26 13:51:20
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for syslog
+-- ----------------------------
+DROP TABLE IF EXISTS `syslog`;
+CREATE TABLE `syslog` (
+  `id` bigint(20) NOT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of syslog
+-- ----------------------------
+INSERT INTO `syslog` VALUES ('1', 'test', '2019-01-26 13:31:39');
