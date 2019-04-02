e0dad4b22c1148cd48e108a641fc5bed20f40869
guosheng_zhang <guosheng_zhang@ssic.cn>
2019-04-02 17:31:23 +0800

    添加ribbon以及LB能力

--- hi-mall/hi-mall-web/src/main/java/com/bestaone/himall/config/FeignConfig.java
+++ hi-mall/hi-mall-web/src/main/java/com/bestaone/himall/config/FeignConfig.java
@@ -2,43 +2,59 @@ package com.bestaone.himall.config;
 
 import com.bestaone.himall.api.goods.GoodsApi;
 import com.bestaone.himall.api.order.OrderApi;
-import feign.Request;
-import feign.Retryer;
+import com.netflix.client.ClientFactory;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.config.ConfigurationManager;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.ZoneAwareLoadBalancer;
 import feign.hystrix.HystrixFeign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
-import org.springframework.beans.factory.annotation.Value;
+import feign.ribbon.LBClient;
+import feign.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
 
 @Configuration
 public class FeignConfig {
 
-    @Value("${hiMall.microsvr.goods.url:}")
-    private String goodsUrl;
-
-    @Value("${hiMall.microsvr.order.url:}")
-    private String orderUrl;
+    @Bean
+    RibbonClient ribbonClient() {
+        try {
+            // 加载 ribbon 相关配置，可以抽取出来独立文件放到classpath下
+            ConfigurationManager.loadPropertiesFromResources("application.yml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 自己是想路由规则
+        return RibbonClient.builder().lbClientFactory(clientName -> {
+            IClientConfig config = ClientFactory.getNamedConfig(clientName);
+            ILoadBalancer lb = ClientFactory.getNamedLoadBalancer(clientName);
+            ZoneAwareLoadBalancer zb = (ZoneAwareLoadBalancer) lb;
+            zb.setRule(new RandomRule());
+            return LBClient.create(lb, config);
+        }).build();
+    }
 
     @Bean
-    GoodsApi goodsApi(){
+    GoodsApi goodsApi() {
         return HystrixFeign.builder()
+                .client(ribbonClient())
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
-//                .errorDecoder()
-                .options(new Request.Options(2000, 3500))
-                .retryer(new Retryer.Default(5000, 5000, 3))
-                .target(GoodsApi.class, goodsUrl);
+                .target(GoodsApi.class, "http://goods-svr/goods/api");
     }
 
     @Bean
     OrderApi orderApi(){
         return HystrixFeign.builder()
+                .client(ribbonClient())
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
-                .options(new Request.Options(2000, 3500))
-                .retryer(new Retryer.Default(5000, 5000, 3))
-                .target(OrderApi.class, orderUrl);
+                .target(OrderApi.class, "http://order-svr/order/api");
     }
 
 }
