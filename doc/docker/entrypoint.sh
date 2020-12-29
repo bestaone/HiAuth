6c5a8b245f786811c78566a3100273ebf725ac58
zhangguosheng@worktrans.cn <117919482>
2020-12-29 14:58:57 +0800

    修改dockerfile

--- doc/docker/entrypoint.sh
+++ doc/docker/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+nohup java -jar /hi-auth-web.jar -Xmx512m -Xss256k >/dev/null &
 nohup java -jar /hi-mall-web.jar -Xmx512m -Xss256k >/dev/null &
 nohup java -jar /hi-mall-microsvr-goods.jar -Xmx512m -Xss256k >/dev/null &
 nohup java -jar /hi-mall-microsvr-order.jar -Xmx512m -Xss256k >/dev/null &
