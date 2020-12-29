d3d81795cd6cc05a56a79a942072455d13845366
zhangguosheng@worktrans.cn <117919482>
2020-12-29 15:09:36 +0800

    修改dockerfile

--- doc/docker/entrypoint.sh
+++ doc/docker/entrypoint.sh
@@ -3,3 +3,7 @@ nohup java -jar /hi-auth-web.jar -Xmx512m -Xss256k >/dev/null &
 nohup java -jar /hi-mall-web.jar -Xmx512m -Xss256k >/dev/null &
 nohup java -jar /hi-mall-microsvr-goods.jar -Xmx512m -Xss256k >/dev/null &
 nohup java -jar /hi-mall-microsvr-order.jar -Xmx512m -Xss256k >/dev/null &
+# 死循环，保持docker前台运行
+while [[ true ]]; do
+    sleep 1
+done
